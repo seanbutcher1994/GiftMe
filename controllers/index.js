a2164626d45f501d8c0d2b3415e5ab94a0c1a0c8
Katherine Ge <gewenyan001@hotmail.com>
2022-11-27 22:09:47 +0800

    routes

--- controllers/index.js
+++ controllers/index.js
@@ -2,14 +2,11 @@ const router = require('express').Router();
 
 const homeRoutes = require('./home-routes');
 const profileRoutes = require('./profile-routes.js');
-const feedRoutes = require('./feed-routes.js')
 const apiRoutes = require('./api');
 
 router.use('/', homeRoutes);
 
 router.use('/profile', profileRoutes);
-
-router.use('/feed', feedRoutes);
 
 router.use('/api', apiRoutes);
 
