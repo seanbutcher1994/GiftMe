fcd01999c393e306cb15cc29c1d7b0aab12f2f04
seanbutcher1994 <110334815+seanbutcher1994@users.noreply.github.com>
2022-12-06 21:50:15 +0800

    more heroku tests

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -14,7 +14,7 @@ router.get("/", async (req, res) => {
   //   .catch((err) => {
   //     res.status(500).json(err);
   //   });
-  res.json('test / route')
+  res.status(200);
 });
 
 router.get("/feed", async (req, res) => {
